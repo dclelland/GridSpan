42416f16db8406a0509adad35f52083bacc94e62
Daniel Clelland <daniel.clelland@gmail.com>
2017-03-26 13:29:16 +1300

    Setup and boilerplate

---
+++ GridRect.swift
@@ -0,0 +1,7 @@
+//
+//  GridRect.swift
+//  Pods
+//
+//  Created by Daniel Clelland on 11/03/17.
+//
+
---
+++ README.md
@@ -0,0 +1,4 @@
+GridRect
+========
+
+`GridRect` is a set of screen scale aware grid layout helpers.
